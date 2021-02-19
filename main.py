64dfc1417c20e4299de3a1ad85ab91664702b6a4
Goochy12 <liamgooch@live.com.au>
2021-02-19 16:09:55 +1100

    fix next page method

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-# 1. open website
-# 2. identify player
-# 3. iterate through cards
-# 4. extract purchases
 
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 url = 'https://evaluate.market/editions'
 playerSelectXPath = '//*[@id="root"]/div/section/main/div/div/div/div/div[1]/div/div[2]/div/div[2]/div[2]/div/div'
 rowCounterXpath = '//*[@id="rc-tabs-0-panel-1"]/div/div/div/ul/li[10]/div/div[1]'
 numberOfRowsXPath = '//*[@id="rc-tabs-0-panel-1"]/div/div/div/ul/li[10]/div/div[2]/div/div/div/div[2]/div/div/div/div[5]'
-nextPageButtonXPathL4 = '//*[@id="rc-tabs-0-panel-1"]/div/div/div/ul/li[9]/button'
-nextPageButtonXPath4 = '//*[@id="rc-tabs-0-panel-1"]/div/div/div/ul/li[10]/button'
-nextPageButtonXPathG4 = '//*[@id="rc-tabs-0-panel-1"]/div/div/div/ul/li[11]/button'
+nextPageButtonClass = 'ant-pagination-next'
+nextPageButtonDisabledClass = 'ant-pagination-disabled'
 tableXPath = '//*[@id="rc-tabs-0-panel-1"]/div/div/div/div/div/div/table'
 dummyRowXPath = '//*[@id="rc-tabs-0-panel-1"]/div/div/div/div/div/div/table/tbody/tr[2]'
 spinnerClassName = 'ant-spin-blur'
@@ -37,7 +30,6 @@ def getPlayerNames(mui, playerDropdownInput):
     return players
 
 def getPlayerDropdownInput(playerSelectXPath):
-    # wait until present
     playerDropdown = wait.until(EC.presence_of_element_located((By.XPATH,playerSelectXPath)))
     playerDropdownInput = playerDropdown.find_element_by_tag_name('input')
     return playerDropdownInput
@@ -59,17 +51,13 @@ def changeRowCount(rowCounterXpath, numberOfRowsXPath):
     wait.until(EC.element_to_be_clickable((By.XPATH,numberOfRowsXPath))).click()
     return
 
-def nextPageInTable(pageNumber):
-    nextPageButtonXPath = None
-    if pageNumber < 3:
-        nextPageButtonXPath = nextPageButtonXPathL4
-    elif pageNumber == 3:
-        nextPageButtonXPath = nextPageButtonXPath4
-    else:
-        nextPageButtonXPath = nextPageButtonXPathG4
-
+def nextPageInTable():
     wait.until(EC.invisibility_of_element_located((By.CLASS_NAME,spinnerClassName)))
-    wait.until(EC.element_to_be_clickable((By.XPATH, nextPageButtonXPath))).click()
+    nextButton = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, nextPageButtonClass)))
+    if(nextButton.get_attribute("aria-disabled")) == "true":
+        return False
+    nextButton.click()
+    return True
 
 def getTableRows(tableXPath):
     table = wait.until(EC.presence_of_element_located((By.XPATH, tableXPath)))
@@ -113,17 +101,12 @@ if __name__ == '__main__':
 
     changeRowCount(rowCounterXpath, numberOfRowsXPath)
 
-    i = 1
-    for i in range(25):
-        nextPageInTable(i)
-        i += 1
+    flag = True
+    while flag == True:
+        flag = nextPageInTable()
 
     # rows = getTableRows(tableXPath)
     # for eachRow in rows:
     #     col = eachRow.find_elements_by_tag_name('td')
     #     for eachCol in col:
     #         print(eachCol.text)
-
-    # nextPageInTable(nextPageButtonXPath)
-
-    # browser.find_element_by_xpath('//*[@id="rc-tabs-0-panel-1"]/div/div/div/ul/li[10]/div/div[1]').click()
